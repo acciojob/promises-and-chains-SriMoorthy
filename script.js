80e8264c97d23b19eaf9721e4bb534dcce07e1f7
SriMoorthy <srimoorthy27@gmail.com>
2024-09-03 13:25:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let submit = document.getElementById('btn');
+let nm = document.getElementById('name')
+let ag = document.getElementById('age')
+let promise = new promise (function(resolve,reject){
+	submit.addEventListener('click', () => {
+		if(ag.innerHTML > 18){
+			resolve(`Welcome, ${nm.innerHTML}. You can vote.`)
+		}
+		else{
+			reject(`Oh sorry ${nm.innerHTML}. You aren't old enough.`)
+		}
+	})
+})
+
+promise.
+	then(function(success){
+		alert(success);
+	},function(error){
+		alert(error);
+	}
